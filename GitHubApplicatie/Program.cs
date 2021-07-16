b79109c1b21ac9f8e6d1e075695e0d657e84240f
Kenny Bruwier <kenny.bruwier@gmail.com>
2021-07-16 11:32:57 +0100

    Add project files.

---
+++ GitHubApplicatie/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GitHubApplicatie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Leeftijd: ");
+            int leeftijd = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
